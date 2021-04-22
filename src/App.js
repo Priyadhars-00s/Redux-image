c8e4be837f0116aaea208b79c3c145fd75bf742f
Priyadhars-00s <priyadh.10decoders@gmail.com>
2021-04-22 18:49:55 +0530

    codechanges

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles.css";
 import CatImages from "./components/CatImages";
 import Button from "./components/Button";
 import axios from "axios";
+import AntDesign from "./antdesign";
 
 export default function App() {
   const [images, setImages] = useState([]);
@@ -10,16 +11,19 @@ export default function App() {
   const getImages = () => {
     axios
       .get("https://api.thecatapi.com/v1/images/search?limit=3")
-      .then(res => setImages(res.data))
+      .then(res =>setImages(res.data))
+
       .catch(err => console.log(err));
-  };
+     
+   };
 
   return (
     <div className="App">
       <h1>Random Cat Images</h1>
       <p>meow</p>
       <Button getImages={getImages} />
-      <CatImages images={images} />
+      <CatImages images={images} /> 
+      <AntDesign />
     </div>
   );
 }
